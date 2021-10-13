cf1455fb944346b0c622fa93dff71afac1564888
walldas <88waldas@gmail.com>
2021-10-13 12:24:41 +0300

    menulis

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 import time
 from flask import Flask, render_template, url_for, request, redirect,make_response, send_file
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime,date
+from datetime import datetime#,date
 import os
+import ephem
 
 	# dd i nn iii
+	# location = {'longitude':24.4004631,'latitude': 56.0607279}
+	
+def add_moon_phases(days):
+
+	date = datetime.now()
+	
+	f =  [1,ephem.next_full_moon(date).triple()[1],int(ephem.next_full_moon(date).triple()[2])]
+	f2 = [1,ephem.previous_full_moon(date).triple()[1],int(ephem.previous_full_moon(date).triple()[2])]
+	p =  [2,ephem.next_first_quarter_moon(date).triple()[1],int(ephem.next_first_quarter_moon(date).triple()[2])]
+	p2 = [2,ephem.previous_first_quarter_moon(date).triple()[1],int(ephem.previous_first_quarter_moon(date).triple()[2])]
+	j =  [3,ephem.next_new_moon(date).triple()[1],int(ephem.next_new_moon(date).triple()[2])]
+	j2 = [3,ephem.previous_new_moon(date).triple()[1],int(ephem.previous_new_moon(date).triple()[2])]
+	d =  [4,ephem.next_last_quarter_moon(date).triple()[1],int(ephem.next_last_quarter_moon(date).triple()[2])]
+	d2 = [4,ephem.previous_last_quarter_moon(date).triple()[1],int(ephem.previous_last_quarter_moon(date).triple()[2])]
+	moon_phases = [p,f,d,j,p2,f2,d2,j2]
+	
+	for moon in moon_phases:
+		for day in days:
+			if day[1] == moon[2] and day[3] == moon[1]:
+				if moon[0]>0:
+					day[4] = moon[0]
+				break
+				
+	# print("1/4",ephem.next_first_quarter_moon(date))
+	# print("full",ephem.next_full_moon(date))
+	# print("3/4",ephem.next_last_quarter_moon(date))
+	# print("new",ephem.next_new_moon(date))
+	
+	
+	# return days
 
 	
 def i_to_state(i):
@@ -22,9 +53,12 @@ def to_table():
 	for d in range(0,31,1):
 		dd = int(time.time() + d * 60 * 60 * 24)
 		day = int(datetime.fromtimestamp(dd).strftime('%d'))
+		month = int(datetime.fromtimestamp(dd).strftime('%m'))
 		week= datetime.fromtimestamp(dd).isoweekday() - 1
 		i = dd // (60 * 60 * 24) % 8
-		days.append([week,day,i_to_border_color(i)])
+		moon = ""
+		days.append([week, day, i_to_border_color(i),month, moon])
+	add_moon_phases(days)
 	weeks = []
 	week =[]
 	[week.append([wk,"",(0,255,0,0)]) for wk in range(days[0][0])]
